fd9773bd888989ca23c4acb50b24b2f859713de1
aniruddha <aniruddha.kudalkar@gmail.com>
2019-03-01 11:00:45 +0530

    oops : constructor

--- third-day/car.ts
+++ third-day/car.ts
@@ -7,9 +7,21 @@ class Car {
     //behaviour
     //moveForward
 
-    wheel: number // fields or members
-    lights: number
-    name: string
+    /*private wheel: number // fields or members
+    private lights: number
+    private name: string
+
+    constructor(wheel: number = 4, lights: number = 10, name: string = 'car') {
+        this.wheel = wheel
+        this.lights = lights
+        this.name = name
+    }*/
+
+    constructor(
+        private wheel: number,
+        private lights: number,
+        private name: string
+    ) { }
 
     mvFwd() {
         this.wheel = this.wheel + 1
@@ -20,6 +32,9 @@ class Car {
     }
 }
 
-let cr: Car = new Car()
+let cr: Car = new Car(4, 4, 'Cars')
+// cr.lights = 10
 cr.mvFwd()
 cr.mvBk()
+
+new Car(56)
