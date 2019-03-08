2fb466b1455f4f37427102fe5dec4e06520fbca9
aniruddha <aniruddha.kudalkar@gmail.com>
2019-03-08 10:42:19 +0530

    component : deeper understanding of components

--- seventh-day/angular-sample-app/src/app/app.component.ts
+++ seventh-day/angular-sample-app/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+})//decorator
 export class AppComponent {
   title = 'angular-sample-app';
 }
