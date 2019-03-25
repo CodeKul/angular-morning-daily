6049c6874acb50c807797285ebdb7ccac3df79bd
aniruddha <aniruddha.kudalkar@gmail.com>
2019-03-25 12:01:52 +0530

    local references : dom manipulation and local refs

--- seventh-day/angular-sample-app/src/app/inter-component-communication-v1/color-picker.component.ts
+++ seventh-day/angular-sample-app/src/app/inter-component-communication-v1/color-picker.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter,ElementRef, ViewChild } from '@angular/core';
 import { DropEvent } from '../app.component';
 
 @Component({
   selector: 'app-color-picker',
   template: `
-  <div class="dropdown show">
+  <div class="dropdown" (click)="toggleSh()" #drp>
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Dropdown button
     </button>
-    <div class="dropdown-menu show" aria-labelledby="dropdownMenuButton">
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"  [class.show]="isSh">
       <a class="dropdown-item" (click)="pickOne(1)">Red</a>
       <a class="dropdown-item" (click)="pickOne(2)">Green</a>
       <a class="dropdown-item" (click)="pickOne(3)">Blue</a>
     </div>
   </div>
+  <div>
+    <input type="text" #myNm/>
+  </div>
   `,
   styles: []
 })
 export class ColorPickerComponent implements OnInit {
+
+  isSh : boolean = false
+  @ViewChild('myNm')
+  private myNm : ElementRef<any>
+
+  @ViewChild('drp')
+  private slkvjlksdvj : ElementRef<any>
 
   @Output()
   picked: EventEmitter<DropEvent> = new EventEmitter()
@@ -27,7 +37,19 @@ export class ColorPickerComponent implements OnInit {
   ngOnInit() {
   }
 
+  toggleSh() {
+    this.isSh = !this.isSh
+    console.log(this.slkvjlksdvj)
+
+    if(this.isSh) {
+      this.slkvjlksdvj.nativeElement.className = 'dropdown show'
+    }else {
+      this.slkvjlksdvj.nativeElement.className = 'dropdown'
+    }
+  }
+
   pickOne(pos: number) {
+    console.log(this.myNm)
     let evObj: DropEvent = {} as DropEvent
     evObj.position = pos
     if (pos == 1) {
