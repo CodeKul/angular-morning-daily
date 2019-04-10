e4928df989cbe74a0cec679dd372538e1317ff60
aniruddha <aniruddha.kudalkar@gmail.com>
2019-04-10 11:18:46 +0530

    router : router setup

---
+++ seventh-day/routerapp/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+import { AdminComponent } from './admin/admin.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'dash', component: DashboardComponent }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ seventh-day/routerapp/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private router : Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogin(eml: string, pass: string) {
+    console.log(`Login Performed`)
+    this.router.navigate(['dash'])
+  }
+}
